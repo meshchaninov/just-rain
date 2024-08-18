ca8a2829060159f9b75f62fc7104894556fe5e9e
Nikita Meshchaninov <dev@nikita.ninja>
2024-08-19 03:53:17 +0700

    add email to me

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-	import { CircleChevronDown, Pause, Play, X } from 'lucide-svelte';
+	import { CircleChevronDown, Pause, Play, X, Star } from 'lucide-svelte';
 	import { fly } from 'svelte/transition';
 
 	const rainSrc = ['/audio/rain1.mp3', '/audio/rain2.mp3', '/audio/rain3.mp3'];
@@ -12,7 +12,6 @@
 	let menuHidden = $state(false);
 
 	let player;
-
 
 	function setPause() {
 		pause = !pause;
@@ -44,79 +43,81 @@
 	<audio bind:this={player} loop autoplay src={rainSrc[currentAudioSrcIndx]} />
 </section>
 
-<main class="min-w-fit">
-	{#if !menuHidden}
-		<div class="hero min-h-screen">
-			<div class="hero-content flex-col w-full">
-				<div
-					class="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl"
-					transition:fly={{ y: -400 }}
-				>
-					<div class="card-body w-96">
-						<div class="flex justify-end">
-							<button class="btn btn-sm btn-ghost btn-circle" onclick={() => showMenu()}>
-								<X class="w-6 h-6" />
-							</button>
-						</div>
-						<div>
-							<h2 class="text-xl font-bold">Rain control</h2>
-							<div class="flex flex-col justify-items-center">
-								<div class="form-control">
-									<label class="label">
-										<span class="label-text">Volume</span>
-									</label>
-									<input
-										type="range"
-										min="0"
-										max="100"
-										class="range"
-										bind:value={volume}
-										onchange={setVolume}
-									/>
-								</div>
-
-								<div class="form-control">
-									<div class="label">
-										<span class="label-text">Playing</span>
+<div class="min-w-fit  min-h-screen flex flex-col">
+	<main class="h-full flex-1">
+		{#if !menuHidden}
+			<div class="hero h-full">
+				<div class="hero-content flex-col w-full min-h-full">
+					<div
+						class="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl"
+						transition:fly={{ y: -400 }}
+					>
+						<div class="card-body w-96">
+							<div class="flex justify-end">
+								<button class="btn btn-sm btn-ghost btn-circle" onclick={() => showMenu()}>
+									<X class="w-6 h-6" />
+								</button>
+							</div>
+							<div>
+								<h2 class="text-xl font-bold">Rain control</h2>
+								<div class="flex flex-col justify-items-center">
+									<div class="form-control">
+										<label class="label">
+											<span class="label-text">Volume</span>
+										</label>
+										<input
+											type="range"
+											min="0"
+											max="100"
+											class="range"
+											bind:value={volume}
+											onchange={setVolume}
+										/>
 									</div>
 
-									{#if pause}
-										<button class="btn btn-sm" onclick={() => setPause()}>
-											<Play />
-										</button>
-									{:else}
-										<button class="btn btn-sm" onclick={() => setPause()}>
-											<Pause />
-										</button>
-									{/if}
-								</div>
-								<div class="form-control">
-									<label class="label">
-										<span class="label-text">Select audio</span>
-									</label>
-									<div class="flex justify-center gap-2">
-										{#each rainSrc as _, i}
-											<button
-												class="btn btn-sm"
-												class:btn-active={currentAudioSrcIndx === i}
-												disabled={pause}
-												onclick={() => (currentAudioSrcIndx = i)}>{i + 1}</button
-											>
-										{/each}
+									<div class="form-control">
+										<div class="label">
+											<span class="label-text">Playing</span>
+										</div>
+
+										{#if pause}
+											<button class="btn btn-sm" onclick={() => setPause()}>
+												<Play />
+											</button>
+										{:else}
+											<button class="btn btn-sm" onclick={() => setPause()}>
+												<Pause />
+											</button>
+										{/if}
 									</div>
-								</div>
-								<div class="form-control">
-									<label class="label">
-										<span class="label-text">Select background</span>
-									</label>
-									<div class="flex justify-center gap-2">
-										{#each backgroundSrc as _, i}
-											<button
-												class="btn btn-sm"
-												class:btn-active={currentBgSrcIndx === i}
-												onclick={() => (currentBgSrcIndx = i)}>{i + 1}</button
-											>
-										{/each}
+									<div class="form-control">
+										<label class="label">
+											<span class="label-text">Select audio</span>
+										</label>
+										<div class="flex justify-center gap-2">
+											{#each rainSrc as _, i}
+												<button
+													class="btn btn-sm"
+													class:btn-active={currentAudioSrcIndx === i}
+													disabled={pause}
+													onclick={() => (currentAudioSrcIndx = i)}>{i + 1}</button
+												>
+											{/each}
+										</div>
+									</div>
+									<div class="form-control">
+										<label class="label">
+											<span class="label-text">Select background</span>
+										</label>
+										<div class="flex justify-center gap-2">
+											{#each backgroundSrc as _, i}
+												<button
+													class="btn btn-sm"
+													class:btn-active={currentBgSrcIndx === i}
+													onclick={() => (currentBgSrcIndx = i)}>{i + 1}</button
+												>
+											{/each}
+										</div>
 									</div>
 								</div>
 							</div>
@@ -124,16 +125,20 @@
 					</div>
 				</div>
 			</div>
-		</div>
-	{:else}
-		<div class="flex justify-center pt-3">
-			<button
-				class="btn btn-circle btn-ghost"
-				onclick={() => showMenu()}
-				transition:fly={{ y: 20, delay: 500 }}
-			>
-				<CircleChevronDown />
-			</button>
-		</div>
-	{/if}
-</main>
+		{:else}
+			<div class="flex justify-center pt-3">
+				<button
+					class="btn btn-circle btn-ghost"
+					onclick={() => showMenu()}
+					in:fly={{ y: 20, delay: 500 }}
+					out:fly={{ y: 20 }}
+				>
+					<CircleChevronDown />
+				</button>
+			</div>
+		{/if}
+	</main>
+	<footer class="text-center text-xs text-gray-500 p-3">
+		<span>Dev by ✨ <a href="mailto:meshchaninov.n@gmail.com" class="link">Nikita Meshchaninov</a></span>
+	</footer>
+</div>
